fd02bca078b23a9d17308228a22c0754e6fcdf30
PolarBear78 <56362723+PolarBear78@users.noreply.github.com>
2023-07-23 10:01:17 -0500

    Add files via upload

--- Simple GUI.py
+++ Simple GUI.py
@@ -5,22 +5,24 @@ class GUI:
         self.master = master
         master.title("A simple GUI")
 
-        self.label = Label(master, text="A simple GUI!")
+        self.label = Label(master, text="API Query Parser/Interpreter", fg='white', bg='grey', font=("Courier", 15))
         self.label.pack()
+        self.master.geometry("500x270")
+        
 
 
-        self.Functionality_button1 = Button(master, text="API1", command=self.API1)
+        self.Functionality_button1 = Button(master, text="API1", command=self.API1, height=2, width=490, fg='white', bg='black', font=("Courier", 15))
         self.Functionality_button1.pack()
 
-        self.Functionality_button2 = Button(master, text="API2", command=self.API2)
+        self.Functionality_button2 = Button(master, text="API2", command=self.API2, height=2, width=490,bg='white', fg='black', font=("Courier", 15))
         self.Functionality_button2.pack()
 
 
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button = Button(master, text="Greet", command=self.greet, height=2, width=490, fg='white', bg='black', font=("Courier", 15))
         self.greet_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(master, text="Close", command=master.quit, height=2, width=490,bg='white', fg='black', font=("Courier", 15))
         self.close_button.pack()
 
     def greet(self):
